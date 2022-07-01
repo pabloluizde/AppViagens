f29624a8081e404a096803819f60d828bb665aa2
Pablo Luiz <91796722+pabloluizde@users.noreply.github.com>
2022-07-01 12:12:58 -0300

    commit
    add header responsive

--- AppViagens/AppViagens/View/ViewController.swift
+++ AppViagens/AppViagens/View/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 20
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
