ce95eff6101811111c15044e15345c1cd12f45a2
Pablo Luiz <91796722+pabloluizde@users.noreply.github.com>
2022-07-05 17:29:14 -0300

    commit
    add ajustes

--- AppViagens/AppViagens/View/Header/HomeHeader.swift
+++ AppViagens/AppViagens/View/Header/HomeHeader.swift
@@ -20,7 +20,7 @@ class HomeHeader: UIView {
         headerView.backgroundColor = UIColor(red: 30.0/255.0, green: 59.0/255.0, blue: 119.0/255.0, alpha: 1)
         bannerView.layer.cornerRadius = 10
         bannerView.layer.masksToBounds = true
-        headerView.layer.cornerRadius = 500
+        headerView.layer.cornerRadius = UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.phone ? 500 : 200
         headerView.layer.maskedCorners = [.layerMinXMaxYCorner,.layerMaxXMaxYCorner]
     }
     
