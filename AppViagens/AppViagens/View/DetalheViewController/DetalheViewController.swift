a218d1f6066a61f490dea2ca0b0e6a0a52c0c757
Pablo Luiz <91796722+pabloluizde@users.noreply.github.com>
2022-07-07 16:16:21 -0300

    commit
    add scroolview

---
+++ AppViagens/AppViagens/View/DetalheViewController/DetalheViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetalheViewController.swift
+//  AppViagens
+//
+//  Created by Pablo Luiz on 07/07/22.
+//
+
+import UIKit
+
+class DetalheViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
